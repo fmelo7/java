6a6e6f6c803b5a961ded15a9f66cc6a48f65c283
Francisco Melo <fmelo7@gmail.com>
2017-12-23 11:27:56 -0200

    adding strategy to create a model

--- pingpong/src/pingpong/model/PingpongEnum.java
+++ pingpong/src/pingpong/model/PingpongEnum.java
@@ -1,5 +1,32 @@
 package pingpong.model;
 
 public enum PingpongEnum {
-	PING, PONG;
+
+	PING(3), //
+	PONG(5);
+
+	private Integer number;
+
+	private PingpongEnum(Integer number) {
+		this.number = number;
+	}
+
+	public static boolean isPing(Integer number) {
+		return number % PING.number == 0;
+	}
+
+	public static boolean isPong(Integer number) {
+		return number % PONG.number == 0;
+	}
+
+	public static PingpongVo createVo(Integer number) {
+		PingpongVo vo = new PingpongVo(number);
+		if (isPing(number)) {
+			vo.getSet().add(PING);
+		}
+		if (isPong(number)) {
+			vo.getSet().add(PONG);
+		}
+		return vo;
+	}
 }
--- pingpong/src/pingpong/v4/Main.java
+++ pingpong/src/pingpong/v4/Main.java
@@ -2,6 +2,7 @@ package pingpong.v4;
 
 import static pingpong.model.PingpongEnum.PING;
 import static pingpong.model.PingpongEnum.PONG;
+import static pingpong.model.PingpongEnum.createVo;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,39 +28,36 @@ public class Main {
 		Assert.assertTrue(list2.contains(new PingpongVo(3, PING)));
 		Assert.assertTrue(list2.contains(new PingpongVo(5, PONG)));
 		Assert.assertTrue(list2.contains(new PingpongVo(15, PING, PONG)));
+		System.out.println("Success!");
 	}
 
 	private static List<PingpongVo> pingpong(List<Integer> list) {
 		return list.stream()//
-				.map(new Function<Integer, PingpongVo>() {
-					@Override
-					public PingpongVo apply(Integer n) {
-						PingpongVo vo = new PingpongVo();
-						vo.setNumber(n);
-						if (n % 3 == 0) {
-							vo.getSet().add(PING);
-						}
-						if (n % 5 == 0) {
-							vo.getSet().add(PONG);
-						}
-						return vo;
-					}
-				})//
+				.map(createVo)//
 				.collect(Collectors.toList());
 	}
 
 	private static List<Integer> initialize(Integer number) {
 		return Arrays//
 				.stream(new Integer[number])//
-				.map(new Function<Integer, Integer>() {
-					private int i = 0;
-
-					@Override
-					public Integer apply(Integer n) {
-						return ++i;
-					}
-				})//
+				.map(fill)//
 				.collect(Collectors.<Integer>toList());
 	}
+
+	private static Function<Integer, PingpongVo> createVo = new Function<Integer, PingpongVo>() {
+		@Override
+		public PingpongVo apply(Integer number) {
+			return createVo(number);
+		}
+	};
+
+	private static Function<Integer, Integer> fill = new Function<Integer, Integer>() {
+		private int i = 0;
+
+		@Override
+		public Integer apply(Integer n) {
+			return ++i;
+		}
+	};
 
 }
