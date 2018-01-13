4fd4fbe53bb051e6eeb5d71619959e73d5aaa75f
Francisco Melo <oi404170@localhost.localdomain>
2018-01-13 12:06:14 -0200

    fixing import error

--- jpa-data-rest-h2-example/src/main/java/com/example/app/Application.java
+++ jpa-data-rest-h2-example/src/main/java/com/example/app/Application.java
@@ -1,42 +1,39 @@
 package com.example.app;
 
+import com.example.app.entity.Customer;
+import com.example.app.entity.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.context.annotation.Bean;
-
-import com.example.app.entity.Customer;
-import com.example.app.entity.CustomerRepository;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@AutoConfigureDataJpa
 public class Application {
 
-	@Autowired
-	protected CustomerRepository customerRepository;
+    @Autowired
+    protected CustomerRepository customerRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
 
-			System.out.println("running application...");
+            System.out.println("running application...");
 
-			customerRepository.deleteAll();
+            customerRepository.deleteAll();
 
-			customerRepository.save(new Customer("Francisco", "Melo"));
-			customerRepository.save(new Customer("Laura", "Mendes"));
-			customerRepository.save(new Customer("John", "Mc Lann"));
-			customerRepository.save(new Customer("Samuel", "jackson"));
+            customerRepository.save(new Customer("Francisco", "Melo"));
+            customerRepository.save(new Customer("Laura", "Mendes"));
+            customerRepository.save(new Customer("John", "Mc Lann"));
+            customerRepository.save(new Customer("Samuel", "jackson"));
 
-			customerRepository.findAll().stream().forEach(System.out::println);
-		};
-	}
+            customerRepository.findAll().stream().forEach(System.out::println);
+        };
+    }
 }
--- jpa-data-rest-h2-example/src/main/java/com/example/app/web/CustomerController.java
+++ jpa-data-rest-h2-example/src/main/java/com/example/app/web/CustomerController.java
@@ -1,26 +1,26 @@
 package com.example.app.web;
 
-import java.util.List;
-
+import com.example.app.entity.Customer;
+import com.example.app.entity.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.app.entity.Customer;
-import com.example.app.entity.CustomerRepository;
+import java.util.List;
 
 @RestController
-@RequestMapping("customers")
+@RequestMapping(value = "customers", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CustomerController {
 
-	@Autowired
-	private CustomerRepository customerRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
-	@GetMapping
-	public ResponseEntity<List<Customer>> getAll() {
-		return ResponseEntity.ok(customerRepository.findAll());
-	}
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAll() {
+        return ResponseEntity.ok(customerRepository.findAll());
+    }
 
 }
